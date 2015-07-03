7ca551ab2fd3125f9ec8729876c761548a00b304
Zack Scholl <zack.scholl@gmail.com>
2015-07-03 12:58:16 -0400

    Final version

--- README.md
+++ README.md
@@ -1,19 +1,89 @@
-# Check How Often I Check Every Site (CHOICES)
+# Check (i.e. *restrain*) How Often I Check (i.e. *inquiry*) Every Site (CHOICES)
 
-Instead of a bookmark manager, this is a tool that hosts all your bookmarks - but only shows the ones that you haven't checked recently. Bookmarks can be set to check every day, week, month, year - any number of times.
+This is a tool that is part bookmark manager, part notification system. It is a convenient webpage that you can set to your "Home" page that consists of your bookmarks sorted in categories. However, the bookmarks will disappear based on the number of times you click on them (which is user-configurable) and then they will reappear at a given frequency (also user-configurable). **Thus, it automatically keeps track of when sites are updated and lets you know - and at the same time it makes sure you don't end up visiting the same site over and over every day or over the week.** It also includes an interesting quote - because why not?
 
-# To-Do
+## How does it work?
 
-- ~~Automatic update with config.json is update - update a statefile for the app (which carries the timestamp of the last config.json and all the important variables)~~
-- ~~Make mobile ready~~
-- ~~Add CSS to lists~~
-- Add random quote from http://www.quotationspage.com/random.php3 in the top
-- Handle case when their are no links at all
-- Set ```last_checked``` to ```floor(time.time()/(60*60*24))```, so that it will easy to determine when its the next day
+This uses [Flask](http://flask.pocoo.org/) for the web-backend. When a request is made, it uses a confg file - ```config.json``` to reload new bookmarks into the state file ```data/state.json```. This state file contains all the information about how often sites are checked, when they were last checked, how often to update, etc. This is very fast (milliseconds) but relatively simple (i.e. their are probably rampant race condition problems if you want to have concurrency).
 
-# To run
+## The configuration file - ```config.json```
 
-Add this NGINX configuration:
+The web site loads in the bookmarks from the ```config.json```. This config file holds the categories, pages, and then parameters for how often to reload them (daily, weekly, monthly) and how many checks are allowed during an interval. An example config.json file looks like this: 
+
+```javascript
+{
+    "Science": {
+        "PNAS": {
+            "checksAvailable": 2,
+            "frequency": "wednesday",
+            "url": "http://www.pnas.org/"
+        },
+        "Nature": {
+            "checksAvailable": 2,
+            "frequency": "thursday",
+            "url": "http://www.nature.com/nature/current_issue.html"
+        },
+        "Science": {
+            "checksAvailable": 2,
+            "frequency": "friday",
+            "url": "http://www.sciencemag.org/content/current"
+        }
+    },
+    "News": {
+        "Hacker News": {
+            "checksAvailable": 1,
+            "frequency": "daily",
+            "url": "https://news.ycombinator.com/"
+        },
+        "Google News": {
+            "checksAvailable": 2,
+            "frequency": "daily",
+            "url": "https://news.google.com/"
+        },
+        "New York Times - Science": {
+            "checksAvailable": 1,
+            "frequency": "weekly",
+            "url": "http://www.nytimes.com/section/science"
+        },
+        "NPR": {
+            "checksAvailable": 1,
+            "frequency": "daily",
+            "url": "http://www.npr.org/sections/news/"
+        }
+    }
+}
+```
+
+This loads in the bookmarks as two categories - "Science" and "News" and loads the individual bookmarks in each. The number of checks are ```checksAvailable``` and the frequency is given. The frequencies that you can use are
+
+```bash
+"daily" - reloaded each day
+"weekly" - reloaded each Sunday
+"monthly" - reloaded on the 10th of each month
+"day of week" - reloaded on the day of week (monday, tuesday, etc.)
+"X" - reloaded on the Xth of each month
+```
+
+## Installation
+
+To install, make a config file by copying the basic one or making your own
+
+```bash
+cp config.json.default config.json
+```
+
+Then you can start the Flask server with
+
+```bash
+python choices.py
+```
+
+That's it!
+
+### NGINX reverse-proxying with Gunicorn
+
+If you want to host using a reverse proxy with Gunicorn, use the following NGINX conf:
+
 
 ```bash	
 server {
@@ -41,3 +111,14 @@ Then run with
 ```bash
 nohup gunicorn -b 127.0.0.1:8009 -w 1 -t 600 choices:app &
 ```
+
+
+# To-Do
+
+- ~~Automatic update with config.json is update - update a statefile for the app (which carries the timestamp of the last config.json and all the important variables)~~
+- ~~Make mobile ready~~
+- ~~Add CSS to lists~~
+- ~~Add random quote from http://www.quotationspage.com/random.php3 in the top~~
+- ~~Set ```last_checked``` to ```floor(time.time()/(60*60*24))```, so that it will easy to determine when its the next day~~
+- Handle case when their are no links at all
+
--- choices.py
+++ choices.py
@@ -3,30 +3,32 @@ import json
 from datetime import datetime
 import time
 import os
-import filecmp
+import random
+from collections import OrderedDict
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
+def random_line(afile):
+    line = next(afile)
+    for num, aline in enumerate(afile):
+      if random.randrange(num + 2): continue
+      line = aline
+    return line
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-	start = time.time()
-	state = {}
+def generate_navigation(path):
+	state = OrderedDict()
 	if not os.path.exists('data'):
 		os.makedirs('data')
 	
-	print state
-	print time.ctime(os.path.getmtime('config.json'))
 	if not os.path.isfile('data/state.json'):
-		bookmarks = json.load(open('config.json','r'))
+		bookmarks = OrderedDict()
+		bookmarks = json.load(open('config.json','r'),object_pairs_hook=OrderedDict)
+
 		for category in bookmarks:
 			for page in bookmarks[category]:
 				
 				# check if everything has time stamp
 				if "last_checked" not in bookmarks[category][page]:
-					bookmarks[category][page]['last_checked'] = time.time()
-					json.dump(bookmarks,open('data/bookmarks.json','w'),sort_keys=True,indent=4, separators=(',', ': '))
+					bookmarks[category][page]['last_checked'] = int(time.time()/(60*60*24))
+					json.dump(bookmarks,open('data/bookmarks.json','w'),indent=4, separators=(',', ': '))
 				
 				if "checks" not in bookmarks[category][page]:
 					bookmarks[category][page]['checks'] = bookmarks[category][page]['checksAvailable']
@@ -35,13 +37,12 @@ def catch_all(path):
 		json.dump(state,open('data/state.json','w'),2)
 	
 	# Load new state
-	state = json.load(open('data/state.json','r'))
-	print state
+	state = json.load(open('data/state.json','r'),object_pairs_hook=OrderedDict)
 	
 	if 'last_modification' not in state or state['last_modification'] != time.ctime(os.path.getmtime('config.json')):
 		# update current bookmarks
 		bookmarks = state['bookmarks']
-		newbookmarks = json.load(open('config.json','r'))
+		newbookmarks = json.load(open('config.json','r'),object_pairs_hook=OrderedDict)
 		for category in newbookmarks:
 			if category not in bookmarks:
 				bookmarks[category] = {}				
@@ -51,7 +52,7 @@ def catch_all(path):
 				for page in newbookmarks[category]:
 					if page not in bookmarks[category]:
 						bookmarks[category][page] = newbookmarks[category][page]
-						bookmarks[category][page]['last_checked'] = time.time()
+						bookmarks[category][page]['last_checked'] = int(time.time()/(60*60*24))
 						bookmarks[category][page]['checks'] = bookmarks[category][page]['checksAvailable']
 					else:
 						for item in newbookmarks[category][page]:
@@ -76,35 +77,54 @@ def catch_all(path):
 	
 	
 	redirectUrl = ''
-	navigation = {}
+	navigation = OrderedDict()
 	bookmarks = state['bookmarks']
 	for category in bookmarks:
 		for page in bookmarks[category]:
 			
 			# Update the checks from checksAvailable if it matches criteria
-			if time.time()-bookmarks[category][page]['last_checked'] > 24*60*60:
+			if int(time.time()/(60*60*24))-bookmarks[category][page]['last_checked'] > 0:
+				print "updating page " + page
 				if bookmarks[category][page]['frequency'] == 'daily' or bookmarks[category][page]['frequency'] == day_of_week or (day_of_week=='sunday' and bookmarks[category][page]['frequency']=='weekly') or bookmarks[category][page]['frequency']==day_of_month or (bookmarks[category][page]['frequency']=='monthly' and day_of_month=='10'):
 					bookmarks[category][page]['checks'] = bookmarks[category][page]['checksAvailable']
-					bookmarks[category][page]['last_checked'] = time.time()
+					bookmarks[category][page]['last_checked'] = int(time.time()/(60*60*24))
 							
 			# Determine whether we need to redirect page
 			if target_category == category and target_name == page:
+				print "redirecting page " + page
 				redirectUrl = bookmarks[category][page]['url']
 				bookmarks[category][page]['checks'] = bookmarks[category][page]['checks'] - 1
-				break
+				print bookmarks[category][page]['checks']
+
 			if bookmarks[category][page]['checks'] > 0:
 				if category not in navigation:
 					navigation[category] = []
 				navigation[category].append({'url':'/' + category + '/' + page, 'name':page})
 	
 	state['bookmarks']=bookmarks
-	json.dump(state,open('data/state.json','w'),sort_keys=True,indent=4, separators=(',', ': '))
+	json.dump(state,open('data/state.json','w'),indent=4, separators=(',', ': '))
+	return navigation,redirectUrl
 
-	print "Took ",time.time()-start
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+	start = time.time()
+	redirectUrl = ''
+	if 'favicon.ico' in path:
+		navigation = {}
+		quote = ''
+	else:
+		navigation,redirectUrl = generate_navigation(path)
+		quote = random_line(open('data/quotes.txt'))
+		
+	#print "Took ",time.time()-start
 	if len(redirectUrl)>0:
 		return redirect(redirectUrl, code=302)
 	else:
-		return render_template('index.html',navigation=navigation)
+		return render_template('index.html',navigation=navigation,quote=quote)
 
 if __name__ == '__main__':
     app.run(host='152.3.53.178',port=5000)
