4d6a7e8e8f84d569d304efe4afce3aad11ff5f59
Zack Scholl <Zack Scholl>
2015-08-23 14:50:29 -0400

    Started adding form to add in more web pages

--- choices.py
+++ choices.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for
 import json
 from datetime import datetime
 import time
@@ -241,9 +241,27 @@ def generate_navigation(path):
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+def hello():
+    error = None
+    data = {}
+    data['recipes'] = cookbook.getRecipes()
+    data['servings'] = [1,2,3,4,5,6,7,8]
+    data['timelimits'] = ['1 min','5 min','10 min','15 min','30 min','1 hour','2 hour','4 hour','12 hour','2 days','1 week','1 year']
+    if request.method == 'GET':
+        data['recipe_text'] = ['']
+        return render_template('index.html',data=data)
+    else:
+        data['recipe_text'] = cookbook.makeRecipe(request.form['recipe'].lower(), int(request.form['servings']), request.form['timelimit'])
+        data['recipe_text'] = markdown(data['recipe_text'])
+        print(data)
+        return render_template('index.html',data=data)
+        
+@app.route('/', defaults={'path': ''}, methods=['GET','POST'])
+@app.route('/<path:path>', methods=['GET','POST'])
 def catch_all(path):
+    if request.method=='POST':
+        print('posting...')
+        print(request.form)
     start = time.time()
     redirectUrl = ''
     if 'favicon.ico' in path:
